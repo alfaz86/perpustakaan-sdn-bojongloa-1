758830c051b0bd11fbdbe16ab0eea98d617dfaca
alfaz86 <hidecode403@gmail.com>
2025-03-13 04:01:02 +0700

    feature: add filament panel

---
+++ resources/views/filament/footer.blade.php
@@ -0,0 +1,15 @@
+<style>
+    @media (min-width: 1024px) {
+        h1.text-footer {
+            margin-left: 320px !important;
+        }
+        .footer-page {
+            z-index: 0 !important;
+        }
+    }
+</style>
+<footer class="footer-page fixed bottom-0 left-0 z-20 w-full p-4 border-t border-gray-200 shadow flex justify-center items-center" style="background-color: #6477DB !important;">
+    <h1 class="text-center text-2xl font-bold text-footer" style="color: yellow;">
+        "Bacalah Bukumu, Bukalah Masa Depanmu"
+    </h1>
+</footer>
